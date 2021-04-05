8dc17dfc56e6f7e62459d87488ff29c94aed437e
KING-REMNANT <60786444+KING-REMNANT@users.noreply.github.com>
2021-04-05 12:06:50 +0300

    My Resume1

---
+++ REMNANT/gallery.php
@@ -0,0 +1,80 @@
+<?php require_once("include/header.php");?>
+
+<br>
+
+<section id="gallery" class="gallery section">
+  <div class="container-fluid">
+    <div class="section-header">
+                <h2 class="wow fadeInDown animated">Gallery</h2>
+                <p class="wow fadeInDown animated">Our Galaries</p>
+            </div>
+    <div class="row no-gutter">
+      <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="images/demo/100x100.jpg" class="work-box"> <img src="images/demo/100x100.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+             <p><span class="icon icon-magnifying-glass"></span></p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="images/demo/100x100.jpg" class="work-box"> <img src="images/demo/100x100.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p><span class="icon icon-magnifying-glass"></span></p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="images/demo/100x100.jpg" class="work-box"> <img src="images/demo/100x100.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p><span class="icon icon-magnifying-glass"></span></p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="images/demo/100x100.jpg" class="work-box"> <img src="images/demo/100x100.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption"> 
+            <p><span class="icon icon-magnifying-glass"></span></p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="images/demo/100x100.jpg" class="work-box"> <img src="images/demo/100x100.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p><span class="icon icon-magnifying-glass"></span></p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="images/demo/100x100.jpg" class="work-box"> <img src="images/demo/100x100.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p><span class="icon icon-magnifying-glass"></span></p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="images/demo/100x100.jpg" class="work-box"> <img src="images/demo/100x100.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+            <p><span class="icon icon-magnifying-glass"></span></p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 work"> <a href="images/demo/100x100.jpg" class="work-box"> <img src="images/demo/100x100.jpg" alt="">
+        <div class="overlay">
+          <div class="overlay-caption">
+             <p><span class="icon icon-magnifying-glass"></span></p>
+          </div>
+        </div>
+        <!-- overlay --> 
+        </a> </div>
+    </div>
+  </div>
+</section>
+<!-- gallery section --> 
+<?php require_once("include/footer.php")?>
